5b9f6c989b4345dce836a39a40009fce1ee5b2b9
luifreire <lufee20@gmail.com>
2023-02-13 18:25:39 -0300

    feat: list fragment was added

---
+++ app/src/main/java/com/example/mypokedex/pokemon/ui/home/PokemonListFragment.kt
@@ -0,0 +1,30 @@
+package com.example.mypokedex.pokemon.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mypokedex.databinding.FragmentPkmnListBinding
+import com.example.mypokedex.pokemon.api.PokemonDataSourceImpl
+import com.example.mypokedex.pokemon.data.PokemonRepositoryImpl
+
+class PokemonListFragment: Fragment() {
+    lateinit var binding: FragmentPkmnListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPkmnListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        PokemonRepositoryImpl(PokemonDataSourceImpl()).getListOfPokemons {
+            binding.rvPokedexList.adapter = it?.let { it1 -> PokedexListAdapter(it1) }
+
+        }
+    }
+}
